52f95a5ab79ab4e14f193cf9b6bb054965315658
Oksana Lalak <oklalak@gmail.com>
2017-02-08 13:49:47 +0200

    init 	* "Hello World" simple example

---
+++ server.js
@@ -0,0 +1,22 @@
+// to start
+// nodemon if it is installed *or node server.js
+// tsc -p ./ -w or npm start
+
+var express = require('express');
+var mongoose = require('mongoose');
+var server = express();
+
+server.use('/', express.static(__dirname + '/'));
+
+server.listen(8080, () => {
+    console.log('Exmple on port 8080!');
+});
+
+//db connection
+//connect('mongodb://username:password@host:port/database')
+var db = mongoose.connect('mongodb://localhost/myapp', (err, db) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Connected!');
+});
